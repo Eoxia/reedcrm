310a9c1f341b5410e7487e6481910f830f9575f7
Nicolas Domenech <nicolas@eoxia.com>
2025-11-20 11:07:47 +0100

    [Hook] fix: fatal addMoreActionsButtons

--- class/actions_reedcrm.class.php
+++ class/actions_reedcrm.class.php
@@ -208,11 +208,10 @@ class ActionsReedcrm
      *
      * @param  array        $parameters Hook metadatas (context, etc...)
      * @param  CommonObject $object     The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
-     * @param  string       $action     Current action (if set). Generally create or edit or null
      * @return int                      0 < on error, 0 on success, 1 to replace standard code
      * @throws Exception
      */
-    public function addMoreActionsButtons(array $parameters, CommonObject $object, string $action): int
+    public function addMoreActionsButtons(array $parameters, CommonObject $object): int
     {
         global $langs, $user;
 
