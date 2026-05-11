200cfdbd90f0f85b9a9ba7a63333ea9113aa7e3a
Laurent Magnin <laurent1magnin@gmail.com>
2026-05-11 09:27:51 +0200

    Update gitignore

--- scratch/test_conf.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'c:/wamp64/www/dolibarr22/dolibarr/htdocs/master.inc.php';
-echo "PROJECT: " . (isset($conf->project->dir_output) ? $conf->project->dir_output : 'NOT_FOUND') . "\n";
-echo "PROJET: " . (isset($conf->projet->dir_output) ? $conf->projet->dir_output : 'NOT_FOUND') . "\n";
