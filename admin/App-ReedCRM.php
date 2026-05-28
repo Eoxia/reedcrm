b54561f5139d690ece216f4c6dc1590468b4e703
Laurent Magnin <laurent1magnin@gmail.com>
2026-05-28 23:43:07 +0200

    #700 [PWA] feat: implement document status bar and configuration page

---
+++ admin/App-ReedCRM.php
@@ -0,0 +1,195 @@
+<?php
+
+/* Copyright (C) 2026 EVARISK <technique@evarisk.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    admin/App-ReedCRM.php
+ * \ingroup reedcrm
+ * \brief   Progressive web apps configuration page for ReedCRM
+ */
+
+// Load ReedCRM environment
+if (file_exists('../reedcrm.main.inc.php')) {
+    require_once __DIR__ . '/../reedcrm.main.inc.php';
+} elseif (file_exists('../../reedcrm.main.inc.php')) {
+    require_once __DIR__ . '/../../reedcrm.main.inc.php';
+} else {
+    die('Include of reedcrm main fails');
+}
+
+// Load Dolibarr libraries
+require_once TCPDF_PATH . 'tcpdf_barcodes_2d.php';
+require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
+
+// Load Module Libraries
+require_once __DIR__ . '/../lib/reedcrm.lib.php';
+
+// Global variables definitions
+global $conf, $db, $langs, $user;
+
+// Load translation files required by the page
+saturne_load_langs();
+
+// Get parameters
+$action = GETPOST('action', 'alpha');
+
+// Initialize view objects
+$form = new Form($db);
+
+// Security check - Protection if external user
+$permissionToRead = $user->rights->reedcrm->adminpage->read;
+saturne_check_access($permissionToRead);
+
+/*
+ * Actions
+ */
+
+$startUrl = dol_buildpath('custom/reedcrm/view/frontend/quickcreation.php?source=pwa', 3);
+
+if ($action == 'generate_QRCode') {
+    $urlToEncode = GETPOST('urlToEncode');
+    if (empty($urlToEncode)) {
+        $urlToEncode = $startUrl;
+    }
+
+    $barcode = new TCPDF2DBarcode($urlToEncode, 'QRCODE,L');
+
+    dol_mkdir($conf->reedcrm->multidir_output[$conf->entity] . '/pwa/qrcode/');
+    $file = $conf->reedcrm->multidir_output[$conf->entity] . '/pwa/qrcode/' . 'barcode_' . dol_print_date(dol_now(), 'dayhourlog') . '.png';
+
+    $imageData = $barcode->getBarcodePngData();
+    $imageData = imagecreatefromstring($imageData);
+    imagepng($imageData, $file);
+
+    setEventMessage('SavedConfig');
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+/*
+ * View
+ */
+
+$title   = $langs->trans('ModuleSetup', 'ReedCRM');
+$helpUrl = 'FR:Module_ReedCRM';
+
+saturne_header(0, '', $title, $helpUrl);
+
+// Subheader
+$linkBack = '<a href="' . DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1' . '">' . $langs->trans('BackToModuleList') . '</a>';
+print load_fiche_titre($title, $linkBack, 'reedcrm_color@reedcrm');
+
+// Configuration header
+$head = reedcrm_admin_prepare_head();
+print dol_get_fiche_head($head, 'pwa', $title, -1, 'reedcrm_color@reedcrm');
+
+print '<a class="marginrightonly" href="' . $startUrl . '" target="_blank">' . img_picto('', 'url', 'class="pictofixedwidth"') . $langs->trans('PWA') . '</a>';
+print showValueWithClipboardCPButton($startUrl, 0, 'none');
+
+// PWA QR Code generation
+print load_fiche_titre($langs->transnoentities('PWAQRCodeGenerationManagement'), '', '');
+
+print '<form method="POST" action="' . $_SERVER['PHP_SELF'] . '">';
+print '<input type="hidden" name="token" value="' . newToken() . '">';
+print '<input type="hidden" name="action" value="generate_QRCode">';
+print '<input hidden name="urlToEncode" value="' . $startUrl . '">';
+
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre">';
+print '<td>' . $langs->trans('Parameters') . '</td>';
+print '<td>' . $langs->trans('Description') . '</td>';
+print '<td class="center">' . $langs->trans('Value') . '</td>';
+print '</tr>';
+
+print '<tr class="oddeven"><td>' . $langs->trans('GeneratePWAQRCode') . '</td>';
+print '<td>' . $langs->trans('GeneratePWAQRCodeDescription') . '</td>';
+print '<td class="center">' . saturne_show_medias_linked('reedcrm', $conf->reedcrm->multidir_output[$conf->entity] . '/pwa/qrcode/', 'small', 1, 0, 0, 0, 80, 80, 0, 0, 0, 'pwa/qrcode/', null, '', 0, 0, 0, 0, 'center') . '</td></tr>';
+
+print '</table>';
+print $form->buttonsSaveCancel('Generate', '');
+print '</form>';
+
+// Configurations
+print '<br>';
+print load_fiche_titre($langs->trans('Config'), '', '');
+
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre">';
+print '<td>' . $langs->trans('Parameters') . '</td>';
+print '<td>' . $langs->trans('Description') . '</td>';
+print '<td class="center">' . $langs->trans('Status') . '</td>';
+print '</tr>';
+
+// App close project when probability zero
+print '<tr class="oddeven"><td>';
+print $langs->trans('AppCloseProjectOpportunityZero');
+print '</td><td>';
+print $langs->trans('AppCloseProjectOpportunityZeroDescription');
+print '</td><td class="center">';
+print ajax_constantonoff('REEDCRM_PWA_CLOSE_PROJECT_WHEN_OPPORTUNITY_ZERO');
+print '</td></tr>';
+
+// Create ActionComm on call list call button
+print '<tr class="oddeven"><td>';
+print $langs->transnoentities('CallListCreateActioncomm');
+print '</td><td>';
+print $langs->transnoentities('CallListCreateActioncommDesc');
+print '</td><td class="center">';
+print ajax_constantonoff('REEDCRM_CALL_LIST_CREATE_ACTIONCOMM');
+print '</td></tr>';
+
+print '</table>';
+
+// Piece display configuration
+print '<br>';
+print load_fiche_titre($langs->trans('PwaShowPiecesTitle'), '', '');
+
+print '<table class="noborder centpercent">';
+print '<tr class="liste_titre">';
+print '<td>' . $langs->trans('Parameters') . '</td>';
+print '<td>' . $langs->trans('Description') . '</td>';
+print '<td class="center">' . $langs->trans('Status') . '</td>';
+print '</tr>';
+
+$pieces = [
+    'REEDCRM_PWA_SHOW_OPP_AMOUNT',
+    'REEDCRM_PWA_SHOW_PROPAL',
+    'REEDCRM_PWA_SHOW_COMMANDE',
+    'REEDCRM_PWA_SHOW_COMMANDE_FOURN',
+    'REEDCRM_PWA_SHOW_RECEPTION',
+    'REEDCRM_PWA_SHOW_FACTURE_FOURN',
+    'REEDCRM_PWA_SHOW_EXPEDITION',
+    'REEDCRM_PWA_SHOW_FACTURE',
+    'REEDCRM_PWA_SHOW_PAYMENT'
+];
+
+foreach ($pieces as $piece) {
+    print '<tr class="oddeven"><td>';
+    print $langs->transnoentities($piece);
+    print '</td><td>';
+    print $langs->transnoentities($piece . '_Desc');
+    print '</td><td class="center">';
+    print ajax_constantonoff($piece);
+    print '</td></tr>';
+}
+
+print '</table>';
+
+// Page end
+print dol_get_fiche_end();
+llxFooter();
+$db->close();
--- lib/reedcrm.lib.php
+++ lib/reedcrm.lib.php
@@ -43,7 +43,7 @@ function reedcrm_admin_prepare_head(): array
     $head[$h][2] = 'settings';
     $h++;
 
-    $head[$h][0] = dol_buildpath('/saturne/admin/pwa.php', 1). '?module_name=ReedCRM&start_url=' . dol_buildpath('custom/reedcrm/view/frontend/quickcreation.php?source=pwa', 3);
+    $head[$h][0] = dol_buildpath('/reedcrm/admin/App-ReedCRM.php', 1);
     $head[$h][1] = $conf->browser->layout != 'phone' ? '<i class="fas fa-mobile pictofixedwidth"></i>' . $langs->trans('App') : '<i class="fas fa-mobile"></i>';
     $head[$h][2] = 'pwa';
     $h++;
@@ -69,3 +69,130 @@ function reedcrm_admin_prepare_head(): array
 
     return $head;
 }
+
+/**
+ * Bulk fetch latest documents for a list of project IDs.
+ *
+ * @param array $projectIds Array of project IDs
+ * @return array Multi-dimensional array: [project_id][doc_type] => ['ref' => ..., 'amount' => ..., 'url' => ...]
+ */
+function reedcrm_get_pwa_projects_documents(array $projectIds): array
+{
+    global $db, $conf;
+
+    $results = [];
+    if (empty($projectIds)) {
+        return $results;
+    }
+
+    $idListStr = implode(',', array_map('intval', $projectIds));
+
+    // Initialize defaults for all project IDs
+    foreach ($projectIds as $id) {
+        $results[$id] = [
+            'montant' => null,
+            'propal' => null,
+            'commande' => null,
+            'commande_fourn' => null,
+            'reception' => null,
+            'facture_fourn' => null,
+            'expedition' => null,
+            'facture' => null,
+            'payment' => null,
+        ];
+    }
+
+    // 1. Montant (Opportunity)
+    if (!empty($conf->global->REEDCRM_PWA_SHOW_OPP_AMOUNT)) {
+        $sql = "SELECT rowid as fk_projet, ref, opp_amount as total_ht FROM " . MAIN_DB_PREFIX . "projet WHERE rowid IN (" . $idListStr . ")";
+        $res = $db->query($sql);
+        if ($res) {
+            while ($row = $db->fetch_object($res)) {
+                $results[$row->fk_projet]['montant'] = [
+                    'ref' => $row->ref,
+                    'amount' => (float)$row->total_ht,
+                    'url' => DOL_URL_ROOT . '/projet/card.php?id=' . $row->fk_projet
+                ];
+            }
+        }
+    }
+
+    // Helper closure to query simple tables
+    $querySimpleTable = function ($constantName, $tableName, $key, $urlPath, $hasAmount = true) use ($db, $idListStr, &$results) {
+        global $conf;
+        if (empty($conf->global->$constantName)) {
+            return;
+        }
+
+        $sql = "SELECT t.fk_projet, t.rowid, t.ref" . ($hasAmount ? ", t.total_ht" : "") . "
+                FROM " . MAIN_DB_PREFIX . $tableName . " t
+                INNER JOIN (
+                    SELECT fk_projet, MAX(rowid) as max_id
+                    FROM " . MAIN_DB_PREFIX . $tableName . "
+                    WHERE fk_projet IN (" . $idListStr . ")
+                    GROUP BY fk_projet
+                ) latest ON t.rowid = latest.max_id";
+
+        $res = $db->query($sql);
+        if ($res) {
+            while ($row = $db->fetch_object($res)) {
+                $results[$row->fk_projet][$key] = [
+                    'ref' => $row->ref,
+                    'amount' => $hasAmount ? (float)$row->total_ht : null,
+                    'url' => DOL_URL_ROOT . $urlPath . $row->rowid
+                ];
+            }
+        }
+    };
+
+    // 2. Propal
+    $querySimpleTable('REEDCRM_PWA_SHOW_PROPAL', 'propal', 'propal', '/comm/propal/card.php?id=');
+
+    // 3. Commande
+    $querySimpleTable('REEDCRM_PWA_SHOW_COMMANDE', 'commande', 'commande', '/commande/card.php?id=');
+
+    // 4. Commande fournisseur
+    $querySimpleTable('REEDCRM_PWA_SHOW_COMMANDE_FOURN', 'commande_fournisseur', 'commande_fourn', '/fourn/commande/card.php?id=');
+
+    // 5. Reception
+    $querySimpleTable('REEDCRM_PWA_SHOW_RECEPTION', 'reception', 'reception', '/reception/card.php?id=', false);
+
+    // 6. Facture fournisseur
+    $querySimpleTable('REEDCRM_PWA_SHOW_FACTURE_FOURN', 'facture_fourn', 'facture_fourn', '/fourn/facture/card.php?id=');
+
+    // 7. Expedition
+    $querySimpleTable('REEDCRM_PWA_SHOW_EXPEDITION', 'expedition', 'expedition', '/expedition/card.php?id=', false);
+
+    // 8. Facture
+    $querySimpleTable('REEDCRM_PWA_SHOW_FACTURE', 'facture', 'facture', '/compta/facture/card.php?id=');
+
+    // 9. Payment (transitively linked via payments on invoices)
+    if (!empty($conf->global->REEDCRM_PWA_SHOW_PAYMENT)) {
+        $sql = "SELECT f.fk_projet, p.ref, p.amount, p.rowid
+                FROM " . MAIN_DB_PREFIX . "paiement p
+                JOIN " . MAIN_DB_PREFIX . "paiement_facture pf ON pf.fk_paiement = p.rowid
+                JOIN " . MAIN_DB_PREFIX . "facture f ON f.rowid = pf.fk_facture
+                INNER JOIN (
+                    SELECT f2.fk_projet, MAX(p2.rowid) as max_id
+                    FROM " . MAIN_DB_PREFIX . "paiement p2
+                    JOIN " . MAIN_DB_PREFIX . "paiement_facture pf2 ON pf2.fk_paiement = p2.rowid
+                    JOIN " . MAIN_DB_PREFIX . "facture f2 ON f2.rowid = pf2.fk_facture
+                    WHERE f2.fk_projet IN (" . $idListStr . ")
+                    GROUP BY f2.fk_projet
+                ) latest ON p.rowid = latest.max_id AND f.fk_projet = latest.fk_projet";
+
+        $res = $db->query($sql);
+        if ($res) {
+            while ($row = $db->fetch_object($res)) {
+                $results[$row->fk_projet]['payment'] = [
+                    'ref' => $row->ref,
+                    'amount' => (float)$row->amount,
+                    'url' => DOL_URL_ROOT . '/compta/paiement/card.php?id=' . $row->rowid
+                ];
+            }
+        }
+    }
+
+    return $results;
+}
+
