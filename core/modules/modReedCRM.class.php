77ac840c958eeca4a59c0fe053428bd1f516e34a
Nicolas Domenech <nicolas@eoxia.com>
2026-06-01 15:17:28 +0200

    #711 [ReedCRM] feat: enable PRODUIT_DESC_IN_FORM on activation (inline line description)

--- core/modules/modReedCRM.class.php
+++ core/modules/modReedCRM.class.php
@@ -906,6 +906,12 @@ class modReedCRM extends DolibarrModules
             }
         }
 
+        // Show product/service description inline under each document line (quotes, orders, invoices, purchase orders).
+        // Migration-safe: do not overwrite a deliberate non-default client choice (0 = Dolibarr default = unconfigured).
+        if (getDolGlobalInt('PRODUIT_DESC_IN_FORM') <= 0) {
+            dolibarr_set_const($this->db, 'PRODUIT_DESC_IN_FORM', '2', 'chaine', 0, '', $conf->entity);
+        }
+
         return $this->_init([], $options);
     }
 
