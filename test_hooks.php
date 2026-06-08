8eb64c6941eb34b99c379d8b8015d34d769f303b
Laurent Magnin <laurent1magnin@gmail.com>
2026-06-08 22:15:22 +0200

    Fix test_hooks.php parameters

--- test_hooks.php
+++ test_hooks.php
@@ -4,22 +4,15 @@ require_once DOL_DOCUMENT_ROOT."/ticket/class/ticket.class.php";
 global $hookmanager, $conf;
 $hookmanager->initHooks(array("ticketcard"));
 echo "<pre>";
-echo "Modules with hooks active:\n";
-print_r($conf->modules_parts["hooks"]);
-echo "\nLoaded contexts:\n";
-print_r($hookmanager->contextarray);
-echo "\nLoaded modules for ticketcard:\n";
-if (isset($hookmanager->hooks["ticketcard"])) {
-    print_r(array_keys($hookmanager->hooks["ticketcard"]));
-} else {
-    echo "None\n";
-}
-echo "\nExecuting formObjectOptions:\n";
+echo "Executing formObjectOptions:\n";
 $object = new Ticket($db);
 $object->fetch(1); // Fetch any ticket, or just fake it
 $action = "view";
-$parameters = array();
+$parameters = array("context" => "ticketcard");
+ob_start();
 $reshook = $hookmanager->executeHooks("formObjectOptions", $parameters, $object, $action);
+$output = ob_get_clean();
 echo "Reshook: " . $reshook . "\n";
 echo "resPrint: " . htmlentities($hookmanager->resPrint) . "\n";
+echo "Direct Output: " . htmlentities($output) . "\n";
 echo "</pre>";
